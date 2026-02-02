fcd1cbb4f99bc8977988a04660f33a8bb3ab4d52
Lukas Bauza <lukasbauza.lb@proton.me>
2026-02-02 08:01:39 +0000

    doom emacs

--- private_dot_config/doom/config.el
+++ private_dot_config/doom/config.el
@@ -28,8 +28,8 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
-(setq doom-font (font-spec :size 16)
-      doom-variable-pitch-font (font-spec :size 18))
+(setq doom-font (font-spec :size 17)
+      doom-variable-pitch-font (font-spec :size 19))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
