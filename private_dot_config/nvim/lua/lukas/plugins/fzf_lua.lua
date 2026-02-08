ffcd4f5a674725daf1de1d5f8f70ba82d516e38f
Lukas Bauza <lukasbauza.lb@proton.me>
2026-02-08 14:26:14 +0000

    fzf-lua plugin

---
+++ private_dot_config/nvim/lua/lukas/plugins/fzf_lua.lua
@@ -0,0 +1,10 @@
+return {
+  "ibhagwan/fzf-lua",
+  name = "fzf-lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  ---@module "fzf-lua"
+  ---@type fzf-lua.config|{}
+  ---@diagnostic disable: missing-fields
+  opts = {}
+  ---@diagnostic enable: missing-fields
+}
