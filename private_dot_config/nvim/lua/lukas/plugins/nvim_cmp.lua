b437f3f04a59f5255256e5e73f8ceb1f7350ee88
Lukas Bauza <lukasbauza.lb@proton.me>
2026-02-26 16:00:10 +0000

    friendly-snippets plugin install.

--- private_dot_config/nvim/lua/lukas/plugins/nvim_cmp.lua
+++ private_dot_config/nvim/lua/lukas/plugins/nvim_cmp.lua
@@ -11,6 +11,7 @@ return {
     "hrsh7th/nvim-cmp",
     "hrsh7th/cmp-nvim-lua",
     "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
     "saadparwaiz1/cmp_luasnip",
     "onsails/lspkind.nvim"
   },
