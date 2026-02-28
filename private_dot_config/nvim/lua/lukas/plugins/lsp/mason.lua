fbe7ff64ae86fdb4dd2ed4c516e4cd1691e300e7
Lukas Bauza <lukasbauza.lb@proton.me>
2026-02-28 09:18:49 +0000

    basedpyright

--- private_dot_config/nvim/lua/lukas/plugins/lsp/mason.lua
+++ private_dot_config/nvim/lua/lukas/plugins/lsp/mason.lua
@@ -1,101 +1,100 @@
 return {
-  "mason-org/mason.nvim",
-  lazy = false,
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "neovim/nvim-lspconfig",
-    { "folke/neodev.nvim", opts = true },
-  },
-  opts = function()
-    require("neodev").setup()
+	"mason-org/mason.nvim",
+	lazy = false,
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"neovim/nvim-lspconfig",
+		{ "folke/neodev.nvim", opts = true },
+	},
+	opts = function()
+		require("neodev").setup()
 
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-    local capabilities = cmp_nvim_lsp.default_capabilities()
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    mason.setup({})
+		mason.setup({})
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        -- c/c++
-        "clangd",
-        -- lua
-        "lua_ls",
-        -- markdown
-        "marksman",
-        -- python
-        "pyright",
-        -- rust
-        "rust_analyzer",
-        -- web
-        "cssls",
-        -- "emmet_ls",
-        "emmet_language_server",
-      },
+		mason_lspconfig.setup({
+			ensure_installed = {
+				-- c/c++
+				"clangd",
+				-- lua
+				"lua_ls",
+				-- markdown
+				"marksman",
+				-- python
+				"basedpyright",
+				-- rust
+				"rust_analyzer",
+				-- web
+				"cssls",
+				-- "emmet_ls",
+				"emmet_language_server",
+			},
 
-      automatic_installation = true
-    })
+			automatic_installation = true,
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        -- c/c++
-        "clangd",
-        -- lua
-        "stylua",
-        -- python
-        "black",
-        "pylint",
-        "isort",
-        -- rust
-        -- web
-        "eslint",
-        "prettier",
-        "typescript-language-server",
-      }
-    })
+		mason_tool_installer.setup({
+			ensure_installed = {
+				-- c/c++
+				"clangd",
+				-- lua
+				"stylua",
+				-- python
+				"black",
+				"pylint",
+				"isort",
+				-- rust
+				-- web
+				"eslint",
+				"prettier",
+				"typescript-language-server",
+			},
+		})
 
-    vim.lsp.config("lua_ls", {
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals =  { "vim" },
-          },
-          completion = {
-            callSnippet = "Replace",
-          },
-          workspace = {
-            library = {
-              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-              [vim.fn.stdpath("config") .. "/lua"] = true,
-            }
-          }
-        }
-      }
-    })
+		vim.lsp.config("lua_ls", {
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+					completion = {
+						callSnippet = "Replace",
+					},
+					workspace = {
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.stdpath("config") .. "/lua"] = true,
+						},
+					},
+				},
+			},
+		})
 
-    vim.lsp.config("emmet_language_server", {
-      capabilities = capabilities,
-      filetypes = {
-        "css",
-        "eruby",
-        "html",
-        "javascript",
-        "javascriptreact",
-        "less",
-        "sass",
-        "scss",
-        "pug"
-      }
-    })
+		vim.lsp.config("emmet_language_server", {
+			capabilities = capabilities,
+			filetypes = {
+				"css",
+				"eruby",
+				"html",
+				"javascript",
+				"javascriptreact",
+				"less",
+				"sass",
+				"scss",
+				"pug",
+			},
+		})
 
-    vim.lsp.enable("lua_ls")
-    vim.lsp.enable("emmet_language_server")
-
-  end
+		vim.lsp.enable("lua_ls")
+		vim.lsp.enable("emmet_language_server")
+	end,
 }
