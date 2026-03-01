ba5ade75517af67f5aaa6cdb7818cae405f3fd9a
Lukas Bauza <lukasbauza.lb@proton.me>
2026-03-01 11:19:37 +0000

    fzf-lua keymaps added

--- private_dot_config/nvim/lua/lukas/plugins/fzf_lua.lua
+++ private_dot_config/nvim/lua/lukas/plugins/fzf_lua.lua
@@ -1,10 +1,33 @@
 return {
-  "ibhagwan/fzf-lua",
-  name = "fzf-lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  ---@module "fzf-lua"
-  ---@type fzf-lua.config|{}
-  ---@diagnostic disable: missing-fields
-  opts = {}
-  ---@diagnostic enable: missing-fields
+	"ibhagwan/fzf-lua",
+	name = "fzf-lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	---@module "fzf-lua"
+	---@type fzf-lua.config|{}
+	---@diagnostic disable: missing-fields
+	opts = {},
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("fzf-lua").files()
+			end,
+			desc = "Find files.",
+		},
+		{
+			"<leader>gl",
+			function()
+				require("fzf-lua").live_grep()
+			end,
+			desc = "Live grep.",
+		},
+		{
+			"<leader>bf",
+			function()
+				require("fzf-lua").buffers()
+			end,
+			desc = "Find buffers.",
+		},
+	},
+	---@diagnostic enable: missing-fields
 }
