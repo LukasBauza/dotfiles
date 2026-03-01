eee323d88ccc61487602d44f12703dc1dfc229b0
Lukas Bauza <lukasbauza.lb@proton.me>
2026-03-01 11:19:58 +0000

    Close window shortcut.

--- private_dot_config/nvim/lua/lukas/settings/keymaps.lua
+++ private_dot_config/nvim/lua/lukas/settings/keymaps.lua
@@ -16,26 +16,30 @@ vim.keymap.set("n", "<Esc>", ":noh<CR>", { desc = "Removes search highlighting w
 
 vim.keymap.set("n", "x", [["_x]], { desc = "Prevents deleted characters from copying to clipboard." })
 
-vim.keymap.set("n", "<leader>er", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-    { desc = "Replace word that is on the cursor everywhere within the file." })
+vim.keymap.set(
+	"n",
+	"<leader>er",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "Replace word that is on the cursor everywhere within the file." }
+)
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-      vim.hl.on_yank()
-  end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
 
 vim.keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically." })
 vim.keymap.set("n", "<leader>wh", "<C-w>s", { desc = "Split window horizontally." })
 vim.keymap.set("n", "<leader>w=", "<C-w>=", { desc = "Make splits equal size." })
-vim.keymap.set("n", "<leader>wx", "<C-w>x", { desc = "Close current split window." })
+vim.keymap.set("n", "<leader>wq", "<C-w>q", { desc = "Close current split window." })
 
 vim.keymap.set("n", "<leader>bx", "<CMD>bdelete<CR>", { desc = "Close the current buffer." })
 
 vim.keymap.set("n", "<leader>fp", function()
-  local filePath = vim.fn.expand("%:~") -- Gets the file path relative to the home directory
-  vim.fn.setreg("+", filePath) -- Copy the file path to the clipboard register
-  print("File path copied to clipboard: " .. filePath)
+	local filePath = vim.fn.expand("%:~") -- Gets the file path relative to the home directory
+	vim.fn.setreg("+", filePath) -- Copy the file path to the clipboard register
+	print("File path copied to clipboard: " .. filePath)
 end, { desc = "Copy file path to clipboard" })
