da608fc1157a554d8fa149256be2181a7879c5e4
Lukas Bauza <lukasbauza.lb@proton.me>
2026-03-08 16:20:38 +0000

    Line width indicator

--- private_dot_config/nvim/lua/lukas/settings/options.lua
+++ private_dot_config/nvim/lua/lukas/settings/options.lua
@@ -5,6 +5,7 @@ vim.opt.cursorline = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.colorcolumn = "80"
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.wrap = true
