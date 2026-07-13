91e9f66f80a13872cb3e68a6298f1d03c3647a5d
Lukas Bauza <lukasbauza.lb@proton.me>
2026-07-13 15:36:09 +0100

    common bash_profile template

---
+++ .chezmoitemplates/common/bash_profile.bash
@@ -0,0 +1,4 @@
+test -z "$PROFILEREAD" && . /etc/profile || true
+export PATH="$PATH:/home/lukas/.emacs.d/bin/"
+export PATH="$PATH:/home/lukas/.local/share/JetBrains/Toolbox/scripts"
+export EDITOR="/usr/bin/nvim"
