7317b81f658dc9cfdd35cbf0f6b100dfb828365f
Lukas Bauza <lukasbauza.lb@proton.me>
2026-07-27 20:20:40 +0100

    bash configuration

--- .chezmoitemplates/common/bash_profile.bash
+++
@@ -1,4 +0,0 @@
-test -z "$PROFILEREAD" && . /etc/profile || true
-export PATH="$PATH:/home/lukas/.emacs.d/bin/"
-export PATH="$PATH:/home/lukas/.local/share/JetBrains/Toolbox/scripts"
-export EDITOR="/usr/bin/nvim"
--- .chezmoitemplates/guix_system/profile.bash
+++
@@ -1,7 +0,0 @@
-# Guix Home (only if present)
-if [ -f "$HOME/.guix-home/setup-environment" ]; then
-  HOME_ENVIRONMENT="$HOME/.guix-home"
-  . "$HOME_ENVIRONMENT/setup-environment"
-  "$HOME_ENVIRONMENT/on-first-login"
-  unset HOME_ENVIRONMENT
-fi
